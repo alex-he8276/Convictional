8e6959fdec2e176042813ae729e495f3eebbbb42
Alex He <alexhe2001@gmail.com>
2021-03-08 20:45:59 -0500

    Added schema

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const port = 3000;
 
 const apiUrl = `https://my-json-server.typicode.com/convictional/engineering-interview/products`;
 let productData;
-
 app.use(express.json());
 
 // Read in product data (if there is a second source of product data, append to productData)
@@ -24,11 +23,7 @@ app.get('/store/inventory', (req, res) => {
   let inventory = [];
   productData.forEach((product) => {
     product.variants.forEach((variant) => {
-      inventory.push({
-        "productId": product.id,
-        "variantId": variant.id,
-        "stock": 0
-      });
+      inventory.push(inventorySchema(variant));
     })
   })
   res.status(200);
@@ -40,7 +35,11 @@ app.get('/store/inventory', (req, res) => {
 // Since '/product/:productId' handles the GET request of a single product
 // Thus this returns all products.
 app.get('/product', (req, res) => {
-  res.json(productData);
+  let newProductData = [];
+  productData.forEach((product) => {
+    newProductData.push(productSchema(product));
+  })
+  res.json(newProductData);
 });
 
 // Returns a single product
@@ -52,12 +51,72 @@ app.get('/product/:productId',
       return res.status(400).send("Invalid ID supplied");
     }
     const productId = Number(req.params.productId);
-      productData.forEach((product) => {
-        if (product.id == productId) {
-          res.status(200);
-          return res.send(product);
-        }
-      })
-      res.status(404);
-      return res.send("product not found");
+    productData.forEach((product) => {
+      if (product.id == productId) {
+        res.status(200);
+        return res.send(productSchema(product));
+      }
+    })
+    res.status(404);
+    return res.send("product not found");
   });
+
+/* function renameKey(obj, oldKey, newKey) {
+  obj[newKey] = obj[oldKey];
+  delete obj[oldKey];
+} */
+
+// Schema for product
+function productSchema(product) {
+  let newProduct = {
+    'code': product.id,
+    'title': product.title,
+    'vendor': product.vendor,
+    'bodyHtml': product.body_html,
+  }
+  let variants = [];
+  if (product.variants) {
+    product.variants.forEach((variant) => {
+      variants.push(variantSchema(variant));
+    })
+  }
+  let images = [];
+  if (product.images) {
+    product.images.forEach((variant) => {
+      images.push(imageSchema(variant));
+    })
+  }
+  newProduct = { ...newProduct, variants, images };
+  return newProduct;
+}
+
+// Schema for variant
+function variantSchema(variant) {
+  return {
+    "id": variant.id,
+    "title": variant.title,
+    "sku": variant.sku,
+    "inventory_quantity": 0,
+    "weight": {
+      "value": variant.weight,
+      "unit": variant.weight_unit,
+    },
+  }
+}
+
+// Schema for image
+function imageSchema(variant) {
+  return {
+    "source": variant.src,
+    "variantId": variant.id,
+  }
+}
+
+// Schema for inventory
+function inventorySchema(variant) {
+  return {
+    "productId": variant.product_id,
+    "variantId": variant.id,
+    "stock": 0
+  }
+}
